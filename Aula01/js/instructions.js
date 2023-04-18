b0eb021efdcc1dedf3fcf79f7edb0a20b06e0823
TimoteoCunha <timoteofragosodacunha@gmail.com>
2023-04-18 16:12:44 +0100

    js

---
+++ Aula01/js/instructions.js
@@ -0,0 +1,9 @@
+let a, b, c; // Declara 3 variaveis
+a = 5;
+b = 6;
+c = a + b;
+
+//document.getElementById("demo").innerHTML = "O valor de A é " + a + ".";
+//document.getElementById("demo").innerHTML = "O valor de B é " + b + ".";
+//document.getElementById("demo").innerHTML = "O valor de C é a soma de A e B " + c + ".";
+document.getElementById("demo").innerHTML = "O valor do C é" + c + ".";
